147b62609b7c6cc0326ea40dbda98de21b30c91c
Jerry Satpathy <rutu.satpathy44@gmail.com>
2018-09-04 23:44:12 +0530

    Pagination Done

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,45 +1,60 @@
-import React, { Component } from 'react'
-export default class Cards extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-items:[],
-ele:[],
-        }
-    }
-    createPages(number){
-        for (let i = 1; i < number; i++) {
-            this.setState(prev => ({
-           ele: [
-                ...prev.ele,
+import React, { Component } from "react";
 
-                  <a
-                    data-id={i}
-                    onClick={e => {
-                      this.fetchNextPage(e.currentTarget.dataset.id);
-                    }}
-                    className="page-link text-dark"
-                  >
-                    {i}
-                  </a>
+export default class Cards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      ele: [],
+      activePage: 15,
+      nextCounter:10
+    };
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+  createPages(number) {
+    this.setState({ ele: [] ,items:[]});
+    for (let i = 1; i < number; i++) {
+      this.setState(prev => ({
+        ele: [...prev.ele, <a key={i} id={i} onClick={e=>console.log(e.target.id)}>{i}</a>]
+      }));
+    }
 
-              ]
-            }));
-        console.log(this.state.ele)
-    }}
-    render(){
-        console.log(this.state.ele)
-        return (
-        <div className="pagination">
-{this.state.ele}
-Fucker
-        </div>)
+  }
+  displayPages(){
+    for(let j=0;j<10;j++){
+        console.log(this.state.ele[j])
+        this.setState(prev=>({
+            items:[...prev.items,this.state.ele[j]]
+        }))
     }
-    componentDidMount(){
-        this.createPages(this.props.pages);
-    }
-    componentWillReceiveProps(props){
-        console.log("Props",props,props.data,props.pages)
-        this.createPages(props.pages);
-    }
+  }
+  createNewPages = index => {};
+  handlePageChange(pageNumber) {
+    console.log(`active page is ${pageNumber}`);
+    this.setState({ activePage: pageNumber });
+  }
+
+  render() {
+      if(!this.props.pages){
+          return ("Type Bitch")
+      }
+    return (
+      <div class="pagination">
+        <a onClick={e=>this.setState({nextCounter:this.state.nextCounter-10})}>&laquo;</a>
+        {this.state.ele.slice(this.state.nextCounter-10,this.state.nextCounter)}
+        <a onClick={e=>this.setState({nextCounter:this.state.nextCounter+10})}>&raquo;</a>
+      </div>
+    );
+  }
+  componentDidMount() {
+    this.createPages(this.props.pages);
+
+
+  }
+  componentWillReceiveProps(props) {
+
+    //  console.log(this.state.ele.length=10)
+    this.createPages(props.pages);
+
+  }
 }
